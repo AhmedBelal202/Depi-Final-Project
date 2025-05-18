8ab68420b63fe0a95db76c68cf88539012bb26d2
Ahmed Belal <ahmed.belal1022@gmail.com>
2025-05-19 00:47:42 +0300

    Update 2_Model.py

--- Streamlit/pages/2_Model.py
+++ Streamlit/pages/2_Model.py
@@ -10,8 +10,8 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 
 # Define function to convert columns to string
-def convert_to_str(X):
-    return X.astype(str)
+def convert_to_str(x):
+    return str(x)
 
 # Set page config
 st.set_page_config(page_title="Sales Prediction App", page_icon="📊")
